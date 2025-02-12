9eba88890560947d2798fda0706dc31eb67ec333
evixsoy <ondra.kosatik1@gmail.com>
2025-02-13 00:01:49 +0100

    rozvrh component + dodelavky

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import Sidebar from './items/Sidebar/Sidebar';
 import Topbar from './items/Topbar/Topbar';
+import Rozvrh from './items/Rozvrh/Rozvrh';
 
 function App() {
-
   return (
-    <div className='Wrapper'>
-      <Sidebar/>
-      <Topbar/>
+    <div className='layout'>
+     <Sidebar/>
+      <div className="window">
+        <Topbar/>
+        <div className="content">
+          <Rozvrh/>
+        </div>
+      </div>
     </div>
   )
 }
